ab72e513f0eeea822beb2cd63a04498a2e93ea3f
“liuhaipeng” <>
2019-01-18 21:19:21 +0800

    优化

--- com.diaosichengxuyuan.one.yuan.brush.ticket.front/app.js
+++ com.diaosichengxuyuan.one.yuan.brush.ticket.front/app.js
@@ -5,12 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejs = require('ejs');
 
-var indexRouter = require('./routes/index');
+var homePageRouter = require('./routes/homePageRouter');
+var ticketSearchRouter = require('./routes/ticketSearch');
 
 var app = express();
 
 // view engine setup
 app.engine('.htm', ejs.__express);
+app.set('view engine', 'htm');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
@@ -19,7 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', homePageRouter);
+app.use('/homePage', homePageRouter);
+app.use('/ticketSearch', ticketSearchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

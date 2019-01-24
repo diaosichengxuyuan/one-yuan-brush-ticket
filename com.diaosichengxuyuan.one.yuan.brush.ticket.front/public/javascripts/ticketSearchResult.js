cd2d358a756ff40fdf9d0745039fa2f03f596532
“liuhaipeng” <>
2019-01-24 22:37:49 +0800

    优化

--- com.diaosichengxuyuan.one.yuan.brush.ticket.front/public/javascripts/ticketSearchResult.js
+++ com.diaosichengxuyuan.one.yuan.brush.ticket.front/public/javascripts/ticketSearchResult.js
@@ -1,18 +1,22 @@
 (
     function (baseSearchUrl) {
-        function TicketSearch(startPlace, endPlace, startDate, isStudent, isHighSpeed, root) {
+        var globalTicketSearch;
+
+        function TicketSearch(startPlace, endPlace, startDate, isStudent, isHighSpeed, trains, root) {
             this.startPlace = startPlace;
             this.endPlace = endPlace;
             this.startDate = startDate;
             this.isStudent = isStudent;
             this.isHighSpeed = isHighSpeed;
+            this.trains = trains;
             this.root = root;
+            this.selectedTickets = [];
         }
 
         TicketSearch.prototype = {
             constructor: TicketSearch,
             getAppendUrl: function () {
-                return startPlace + endPlace + startDate + isStudent + isHighSpeed;
+                return this.startPlace + this.endPlace + this.startDate + this.isStudent + this.isHighSpeed;
             },
             getSearchUrl: function (searchCondition) {
                 return baseSearchUrl + searchCondition;
@@ -30,7 +34,7 @@
                 //     }
                 // });
                 return [{
-                    trains: "G101",
+                    trains: "G102",
                     startTime: "06:43",
                     endTime: "12:40",
                     startStation: "北京南",
@@ -51,7 +55,7 @@
                     softSleeperSeatPrice: "",
                     softSleeperSeatRemain: ""
                 }, {
-                    trains: "G101",
+                    trains: "G103",
                     startTime: "06:43",
                     endTime: "12:40",
                     startStation: "北京南",
@@ -72,7 +76,70 @@
                     softSleeperSeatPrice: "",
                     softSleeperSeatRemain: ""
                 }, {
-                    trains: "G101",
+                    trains: "G104",
+                    startTime: "06:43",
+                    endTime: "12:40",
+                    startStation: "北京南",
+                    endStation: "上海虹桥",
+                    costTime: "5小时57分",
+                    secondSeatPrice: "￥553",
+                    secondeSeatRemain: "21",
+                    firstSeatPrice: "￥772",
+                    firstSeatRemain: "18",
+                    businessSeatPrice: "￥1500",
+                    businessSeatRemain: "0",
+                    noSeatPrice: "",
+                    noSeatRemain: "",
+                    hardSeatPrice: "",
+                    hardSeatRemain: "",
+                    hardSleeperSeatPrice: "",
+                    hardSleeperSeatRemain: "",
+                    softSleeperSeatPrice: "",
+                    softSleeperSeatRemain: ""
+                }, {
+                    trains: "G105",
+                    startTime: "06:43",
+                    endTime: "12:40",
+                    startStation: "北京南",
+                    endStation: "上海虹桥",
+                    costTime: "5小时57分",
+                    secondSeatPrice: "￥553",
+                    secondeSeatRemain: "21",
+                    firstSeatPrice: "￥772",
+                    firstSeatRemain: "18",
+                    businessSeatPrice: "￥1500",
+                    businessSeatRemain: "0",
+                    noSeatPrice: "",
+                    noSeatRemain: "",
+                    hardSeatPrice: "",
+                    hardSeatRemain: "",
+                    hardSleeperSeatPrice: "",
+                    hardSleeperSeatRemain: "",
+                    softSleeperSeatPrice: "",
+                    softSleeperSeatRemain: ""
+                }, {
+                    trains: "G106",
+                    startTime: "06:43",
+                    endTime: "12:40",
+                    startStation: "北京南",
+                    endStation: "上海虹桥",
+                    costTime: "5小时57分",
+                    secondSeatPrice: "￥553",
+                    secondeSeatRemain: "21",
+                    firstSeatPrice: "￥772",
+                    firstSeatRemain: "18",
+                    businessSeatPrice: "￥1500",
+                    businessSeatRemain: "0",
+                    noSeatPrice: "",
+                    noSeatRemain: "",
+                    hardSeatPrice: "",
+                    hardSeatRemain: "",
+                    hardSleeperSeatPrice: "",
+                    hardSleeperSeatRemain: "",
+                    softSleeperSeatPrice: "",
+                    softSleeperSeatRemain: ""
+                }, {
+                    trains: "G107",
                     startTime: "06:43",
                     endTime: "12:40",
                     startStation: "北京南",
@@ -107,8 +174,13 @@
                     '<th class="ticketResultTh">出发站/到达站</th>' +
                     '<th class="ticketResultTh">耗时</th>' +
                     '<th class="ticketResultTh">参考票价</th>' +
-                    '<th class="ticketResultTh">余票</th>' +
-                    '</tr>' +
+                    '<th class="ticketResultTh">余票</th>';
+
+                if (this.trains != undefined && this.trains != "") {
+                    html += '<th class="ticketResultTh">选择</th>';
+                }
+
+                html += '</tr>' +
                     '</thead>' +
                     '<tbody>';
 
@@ -120,14 +192,20 @@
                             '<td class="ticketResultTd">' + ticket.startTime + '/' + ticket.endTime + '</td>' +
                             '<td class="ticketResultTd">' + ticket.startStation + '/' + ticket.endStation + '</td>' +
                             '<td class="ticketResultTd">' + ticket.costTime + '</td>';
+
                         if (ticket.secondSeatPrice != undefined && ticket.secondSeatPrice != "") {
                             html += '<td class="ticketResultTd">二等座 <a class="ticketResultReminder">' + ticket.secondSeatPrice + '</a></br>一等座 <a class="ticketResultReminder">' + ticket.firstSeatPrice + '</a></br>商务座 <a class="ticketResultReminder">' + ticket.businessSeatPrice + '</a></br></td>' +
-                                '<td class="ticketResultTd">余<a class="ticketResultReminder">' + ticket.secondeSeatRemain + '</a>张</br>余<a class="ticketResultReminder">' + ticket.firstSeatRemain + '</a>张</br>余<a class="ticketResultReminder">' + ticket.businessSeatRemain + '</a>张</br></td>' +
-                                '</tr>';
+                                '<td class="ticketResultTd">余<a class="ticketResultReminder">' + ticket.secondeSeatRemain + '</a>张</br>余<a class="ticketResultReminder">' + ticket.firstSeatRemain + '</a>张</br>余<a class="ticketResultReminder">' + ticket.businessSeatRemain + '</a>张</br></td>';
                         }
                         else {
 
                         }
+
+                        if (this.trains != undefined && this.trains != "") {
+                            html += '<td class="ticketResultTd"><input class="ticketResultSelect" type="checkbox"></td>';
+                        }
+
+                        html += '</tr>';
                     }
                 }
 
@@ -138,16 +216,45 @@
             bindToRootElement: function (ticketResultHtml) {
                 var rootElement = document.getElementById(this.root);
                 rootElement.innerHTML = ticketResultHtml;
+            },
+            addSelectListener: function () {
+                $(".ticketResultSelect").click(function () {
+                    if (globalTicketSearch == undefined) {
+                        return;
+                    }
+
+                    var trElement = $(this).parent().parent();
+                    var train = trElement.find("td").eq(0).text();
+                    if ($(this).prop("checked")) {
+                        globalTicketSearch.selectedTickets.push(train);
+                    }
+                    else {
+                        CommonUtils.removeFromArray(globalTicketSearch.selectedTickets, train);
+                    }
+                    document.getElementById(globalTicketSearch.trains).value = globalTicketSearch.selectedTickets.join(";");
+                });
             }
         }
 
         TicketSearch.search = function (startPlace, endPlace, startDate, isStudent, isHighSpeed, root) {
-            var ticketSearch = new TicketSearch(startPlace, endPlace, startDate, isStudent, isHighSpeed, root);
+            var ticketSearch = new TicketSearch(startPlace, endPlace, startDate, isStudent, isHighSpeed, "", root);
+            globalTicketSearch = ticketSearch;
             var appendUrl = ticketSearch.getAppendUrl();
             var searchUrl = ticketSearch.getSearchUrl(appendUrl);
             var ticketList = ticketSearch.search(searchUrl);
             var ticketResultHtml = ticketSearch.generateHtml(ticketList);
             ticketSearch.bindToRootElement(ticketResultHtml);
+        }
+
+        TicketSearch.searchAndCheck = function (startPlace, endPlace, startDate, trains, root) {
+            var ticketSearch = new TicketSearch(startPlace, endPlace, startDate, false, false, trains, root);
+            globalTicketSearch = ticketSearch;
+            var appendUrl = ticketSearch.getAppendUrl();
+            var searchUrl = ticketSearch.getSearchUrl(appendUrl);
+            var ticketList = ticketSearch.search(searchUrl);
+            var ticketResultHtml = ticketSearch.generateHtml(ticketList);
+            ticketSearch.bindToRootElement(ticketResultHtml);
+            ticketSearch.addSelectListener();
         }
 
         window.TicketSearch = TicketSearch;

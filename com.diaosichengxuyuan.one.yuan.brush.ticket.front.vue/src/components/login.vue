42458f72b9359a09e64071d4b13afcd119bbbd84
“liuhaipeng” <>
2019-05-08 21:37:21 +0800

    使用vue改造前端---登录

---
+++ com.diaosichengxuyuan.one.yuan.brush.ticket.front.vue/src/components/login.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <div id="mainLoginArea" v-on:click="hidePlaces">
+      <div class="instruction">请使用12306账号密码登录</div>
+      <div class="accountAndPassword">
+        账号&nbsp;&nbsp;
+        <input id="accountId" type="text" v-model="accountId" autocomplete="off">
+      </div>
+      <div class="accountAndPassword">
+        密码&nbsp;&nbsp;
+        <input id="passwordId" type="password" v-model="password" autocomplete="off">
+      </div>
+      <div class="protocal">
+        <input id="protocalBoxId" type="checkbox" v-model="isAgree">同意
+        <a
+          id="protocalLinkId"
+          class="protocalLink"
+          href="javascript:void(0);"
+          v-on:click.stop="showPlaces"
+        >用户协议</a>
+      </div>
+      <div class="login">
+        <input id="loginId" type="button" value="登录" v-on:click="login">
+      </div>
+    </div>
+    <div id="mainContentArea" v-if="agreementShow">
+      <BrushTicketAgreement></BrushTicketAgreement>
+    </div>
+  </div>
+</template>
+
+<script>
+import BrushTicketAgreement from "@/components/brushTicketAgreement";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      accountId: "",
+      password: "",
+      isAgree: false,
+      agreementShow: false
+    };
+  },
+  methods: {
+    login: function() {
+      this.$router.push({
+        name: "LoginSuccess"
+      });
+    },
+    showPlaces: function() {
+      this.agreementShow = true;
+    },
+    hidePlaces: function(event) {
+      this.agreementShow = false;
+    }
+  },
+  components: {
+    BrushTicketAgreement
+  }
+};
+</script>
+
+<style scoped>
+#mainContentArea /deep/ #mainContentArea {
+  width: 400px;
+  left: 838px;
+}
+
+#mainLoginArea {
+  float: left;
+  width: 1200px;
+  margin-left: -10px;
+  margin-top: 30px;
+  font-size: 20px;
+}
+
+#mainLoginArea .instruction {
+  width: 500px;
+  margin: auto;
+  text-align: center;
+  margin-top: 10px;
+  font-size: 25px;
+}
+
+#mainLoginArea .accountAndPassword {
+  width: 500px;
+  margin: auto;
+  margin-top: 30px;
+  text-align: center;
+}
+
+#mainLoginArea .accountAndPassword input {
+  width: 300px;
+  height: 30px;
+  text-align: center;
+  font-size: 20px;
+  outline: none;
+  border: solid 1.4px;
+  border-color: rgb(170, 164, 164);
+}
+
+#mainLoginArea .protocal {
+  width: 500px;
+  margin: auto;
+  margin-top: 30px;
+  text-align: center;
+}
+
+#mainLoginArea .protocal input {
+  zoom: 220%;
+  vertical-align: middle;
+}
+
+#mainLoginArea .protocal a {
+  color: red;
+  text-decoration: none;
+}
+
+#mainLoginArea .login {
+  width: 500px;
+  margin-top: 30px;
+  margin-left: 380px;
+  text-align: center;
+}
+
+#mainLoginArea .login input {
+  font-size: 20px;
+  background-color: #ff7300;
+  outline: none;
+  width: 300px;
+  height: 35px;
+}
+
+.protocalContent {
+  position: absolute;
+  width: 400px;
+  height: 400px;
+  left: 840px;
+  top: 190px;
+  overflow-y: auto;
+  display: none;
+}
+</style>
---
+++ com.diaosichengxuyuan.one.yuan.brush.ticket.front.vue/src/components/loginSuccess.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="mainLoginArea">
+    <div class="instruction">已登录</div>
+    <div class="account">
+      账号&nbsp;&nbsp;
+      <input id="accountId" type="text" readonly="readonly" v-model="accountId">
+    </div>
+    <div class="loginOut">
+      <input id="loginOutId" type="button" value="退出登录" v-on:click="loginOut">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginSuccess",
+  data() {
+    return {
+      accountId: "121xxxxx26@qq.com"
+    };
+  },
+  methods: {
+    loginOut: function() {
+      this.$router.push({
+        name: "Login"
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+#mainLoginArea {
+  float: left;
+  width: 1200px;
+  margin-left: -10px;
+  margin-top: 30px;
+  font-size: 20px;
+}
+
+#mainLoginArea .instruction {
+  width: 500px;
+  margin: auto;
+  text-align: center;
+  margin-top: 10px;
+  font-size: 25px;
+}
+
+#mainLoginArea .account {
+  width: 500px;
+  margin: auto;
+  margin-top: 30px;
+  text-align: center;
+}
+
+#mainLoginArea .account input {
+  width: 300px;
+  height: 30px;
+  text-align: center;
+  font-size: 20px;
+  background-color: rgb(170, 164, 164);
+}
+
+#mainLoginArea .loginOut {
+  width: 500px;
+  margin-top: 180px;
+  margin-left: 380px;
+  text-align: center;
+}
+
+#mainLoginArea .loginOut input {
+  font-size: 20px;
+  background-color: #ff7300;
+  outline: none;
+  width: 300px;
+  height: 35px;
+}
+</style>
